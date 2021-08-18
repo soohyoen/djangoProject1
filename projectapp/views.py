a3ddd63ae2c72aa7ff124f72f4dfe7ddafcf2e18
soohyoen <gnejfejf789@naver.com>
2021-08-18 09:29:18 +0900

    2021.08.18 게시판 버튼 활성화(test)

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,10 +1,12 @@
+
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
@@ -25,3 +27,9 @@ class ProjectDetailView(DetailView):
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
 
+
+class ProjectListView(ListView):
+    model = Project
+    context_object_name = 'project_list'
+    template_name = 'projectapp/list.html'
+    paginate_by = 20
