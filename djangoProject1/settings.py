45886471ed031cb163a2462508652eb2dbfebc59
soohyoen <gnejfejf789@naver.com>
2021-08-19 16:03:49 +0900

    2021.08.19 Create subscribe app,subscribe button, bug fix

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'articleapp',
     'commentapp',
     'projectapp',
-    'subcribeapp',
+    'subscribeapp',
 ]
 
 MIDDLEWARE = [
--- subscribeapp/apps.py
+++ subscribeapp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SubcribeappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'subcribeapp'
+    name = 'subscribeapp'
